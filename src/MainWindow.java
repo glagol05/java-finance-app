80af8a09482b863d09e08ad189a27d5f234b7678
glagol05 <vehiddemic@gmail.com>
2025-09-21 13:40:19 +0800

    changed strategy for setting up JFrame

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,19 +1,9 @@
 import javax.swing.JFrame;
 
-public class MainWindow {
+public class MainWindow extends JFrame {
     
-    private JFrame window;
-
     public MainWindow() {
-        window = new JFrame();
-        window.setTitle("Finance application");
-        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        window.setSize(800,500);
-        window.setLocationRelativeTo(null);
-    }
-
-    public void show() {
-        window.setVisible(true);
+        initialize();    
     }
 
 
