31e814cf6d9533b82cdd06cd82bdb61de302aac0
glagol05 <vehiddemic@gmail.com>
2025-09-21 17:16:58 +0800

    Added a function where the user can type a short descriptive name of an expense and the cost associated with it.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,8 +1,22 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class MainWindow {
     
     private JFrame frame;
+    private int total = 0;
+    private JLabel balanceLabel;
+    private JLabel expenseLabel;
+    private JButton removeExpense;
 
     public MainWindow() {
         initialize();
@@ -16,6 +30,55 @@ public class MainWindow {
         this.frame.setLocationRelativeTo(null);
         this.frame.setResizable(false);
         this.frame.setVisible(true);
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+        inputPanel.setBackground(Color.RED);
+
+        JTextArea inputDescription = new JTextArea(1,20);
+        inputDescription.setFont(new Font("Arial", Font.PLAIN, 16));
+        inputPanel.add(inputDescription);
+
+        JTextArea inputCost = new JTextArea(1, 20);
+        inputCost.setFont(new Font("Arial", Font.PLAIN, 16));
+        inputPanel.add(inputCost);
+
+        JButton addNumber = new JButton("Add");
+
+        JPanel balancePanel = new JPanel();
+        balancePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+        balancePanel.setBackground(Color.WHITE);
+
+        balanceLabel = new JLabel("Your total balance: " + Integer.toString(total));
+        balancePanel.add(balanceLabel);
+
+        JPanel viewPanel = new JPanel();
+        viewPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+        viewPanel.setBackground(Color.GREEN);
+
+        addNumber.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int value = Integer.parseInt(inputCost.getText());
+                total += value;
+                balanceLabel.setText("Your total balance: " + Integer.toString(total) + " kr");
+
+                String description = inputDescription.getText();
+
+                expenseLabel = new JLabel();
+                expenseLabel.setText(description + ": " + Integer.toString(value) + " kr");
+                viewPanel.add(expenseLabel);
+
+                inputDescription.setText("");
+                inputCost.setText("");
+            }
+        });
+
+        inputPanel.add(addNumber);
+
+        frame.add(inputPanel, BorderLayout.NORTH);
+        frame.add(viewPanel, BorderLayout.CENTER);
+        frame.add(balancePanel, BorderLayout.SOUTH);
     }
 
 }
